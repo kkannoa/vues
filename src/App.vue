3dd83043c09eae408dc1e38950f191bf9f4847be
jun <we2857@gmail.com>
2024-05-05 21:06:27 +0900

    test1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+  <!-- <div v-if="1==2">안녕하세요</div>
+  <div v-else-if="1==3">안녕하세요2</div>
+  <div v-else>안녕하세요3</div>
+ -->
+  <div>
+    
+    
+    <Modal @closeModal="모달창열렸니=false;" :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니" />
+    
+    <div class="menu">
+      <a v-for="(menu) in menus" :key=menu.id>{{ menu.name }}</a>
+      <a>Products</a>
+      <a>About</a>
+    </div>
+
+    <Discount/>
+
+    <Card @openModal="모달창열렸니=true; 누른거=$event" :a="원룸들[i]" v-for="(a , i) in 원룸들" :key=i></Card>
+
+    
+
+  </div>
+
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {data} from './assets/oneroom.js';
+import Discount from './Discount.vue';
+import Modal from './Modal.vue';
+import Card from './Card.vue';
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      오브젝트 : {name : 'kim', age: 20},
+      원룸들 : data,
+      모달창열렸니: false,
+      신고수: [0, 0, 0],
+      menus: [{ id: 1, name: 'Home' }, { id: 2, name: 'Shop' }, { id: 3, name: 'About' }],
+      price: [50, 60, 70],
+      product: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      images: [require('./assets/room0.jpg'), require('./assets/room1.jpg'), require('./assets/room2.jpg')],
+      누른거:0,
+    }
+  },
+  methods: {
+    increase(index) {
+      this.신고수[index] += 1
+    },
+    
+  },
   components: {
-    HelloWorld
+    Discount,
+    Modal,
+    Card,
   }
 }
 </script>
+
 
 <style>
 #app {
@@ -23,4 +73,27 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+body {
+  margin: 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+
+
+
 </style>
---
+++ src/Modal.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="black-bg" v-if="모달창열렸니">
+      <div class="white-bg">
+        <img :src="원룸들[누른거].image">
+        <h4>{{원룸들[누른거].title}}</h4>
+        <p>{{ 원룸들[누른거].content }}</p>
+        
+        <input v-model.number="month"> <!-- 약어 -->
+
+        <p> {{ month }} 개월 선택함 : {{ 원룸들[누른거].price * month }} </p>
+        <p>{{ 원룸들[누른거].price }} 원</p>
+
+        <!-- <input type="checkbox" v-model.number="month"> -->
+
+        <!-- <select v-model="month">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+        </select> -->
+        <p>{{ selectValue }}</p>
+        <button @click="$emit('closeModal')">닫기</button>
+        <!-- <button @click="log()">확인</button> -->
+        
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'ModalComponent',
+    data(){
+        return{
+            month : 1,
+        }
+    },
+    watch : {
+        month(a){
+      if (isNaN(a) == true){
+        alert('문자입력하지마라');
+        this.month = 1;
+      }else if(a>12){
+        alert('13이상 적지마라');
+        this.month = 12;
+      }
+    },
+    },
+    props :{
+        원룸들 : Object, /* 데이터 형식을 적는다 Stirng, Array*/
+        누른거 : Number,
+        모달창열렸니 : Boolean,
+    },
+    methods :{
+        log(){
+            console.log(this.month)
+            
+        }
+    }
+}
+</script>
+
+<style>
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+
+}
+</style>
